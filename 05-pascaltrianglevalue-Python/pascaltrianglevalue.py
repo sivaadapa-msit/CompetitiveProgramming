7569c5c04a321ea27014269f4da0a9b1a5704a95
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-10 17:56:32 +0530

    mod 2 7

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -7,6 +7,16 @@
 
 
 
-
+import math
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	if row < 0 or col < 0 :
+		return None
+	elif row < col:
+		 return 0
+	elif col<= row:
+		 return (math.factorial(row) / (math.factorial(col) * math.factorial(row - col)))
+	else :
+		return None
+
+			
+			
