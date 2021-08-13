0db481d0dedfe9e205397524e4f1b049dc281431
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-13 17:16:49 +0530

    commit 4 r

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -11,7 +11,19 @@
 
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
+	cipher = ''
+	for char in msg:
+		if char == ' ':
+			  cipher = cipher + char
+		elif  char.isupper():
+			  cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+		else:
+			  cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+  
+	return cipher
+
+
+
 
 
 
