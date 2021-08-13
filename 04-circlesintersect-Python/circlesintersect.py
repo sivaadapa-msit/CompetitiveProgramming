573ec4a33d83a3072dd099e041997981e4feb98f
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-13 16:15:43 +0530

    mod 4 2

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -6,4 +6,10 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	return False 
+	
+	dis = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+	rad = (r1 + r2) * (r1 + r2);
+	if dis==rad or dis<rad:
+		return True
+	else:
+		return False
