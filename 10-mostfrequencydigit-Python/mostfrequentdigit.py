858b77a821dac6deaffe1e1999c0993d0b692479
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-17 14:22:35 +0530

    mod 4 6

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,7 +3,22 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
     
+def c(x,d):
+    count=0
+    while(x>0):
+        if(x%10==d):
+            count+=1
+        x=x//10
+    return count
 def mostfrequentdigit(n):
-    
-	# your code goes here
-	
+    n=abs(n)
+    digit=0
+    max=0
+    count=0
+    for i in range(10):
+        count= c(n,i)
+        if(count>max):
+            max=count
+            digit=i
+        
+    return digit
