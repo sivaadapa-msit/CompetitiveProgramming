7dd64d40ccdc7b32f820db6bdc917759b5dfbd4d
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-18 20:49:37 +0530

    mod 7 4

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -5,6 +5,47 @@
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 
 
-def nthpowerfulnumber(n):
+
+    
+def isprime(x):
+    count=0
+    if x >1 :
+        for i in range(1,x+1):
+            if x%i==0:
+               count=count+1
+        if count==2:
+            return True
+        else:
+            return False       
+                
+                
+    
 	# Your code goes here
-	pass
+	
+def powerfulnumber(x):
+    if x==1:
+        return True
+    else:
+        count=0
+        count2=0
+        for i in range(2,int(x/2)+1):
+            if(x%i==0 and isprime(i)):
+                count+=1
+                if(x%(i*i)==0):
+                    count2+=1
+        print(count,count2)
+        if(count==0 or count2==0):
+            return False
+        elif(count==count2):
+            return True
+
+    return False
+def nthpowerfulnumber(n):
+    
+    found = 0
+    guess = 0
+    while (found <= abs(n)):
+        guess += 1
+        if(powerfulnumber(guess)):
+            found += 1
+    return guess
