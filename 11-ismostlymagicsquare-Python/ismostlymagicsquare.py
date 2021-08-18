2975b84d2cd58ff0d55b3e61bdf2159e8d116728
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-18 15:25:18 +0530

    commit 6 6

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -13,6 +13,25 @@
 #   [ 2, 1]]
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
-def ismostlymagicsquare(a):
-	# Your code goes here
-	pass
+def ismostlymagicsquare(arr):
+    leftdiag=0
+    rightdiag=0
+    n=len(arr)
+    #to check diagonals 
+    for i in range(n):
+        leftdiag +=arr[i][i]
+        rightdiag+=arr[i][n-i-1]
+    if leftdiag != rightdiag:
+        return False
+    
+    #To check row and col
+    for i in range(n):
+        row=0
+        col=0
+        for j in range(n):
+            row+=arr[i][i]
+            col+=arr[j][i]
+            
+        if row!= leftdiag and col!=leftdiag:
+            return False
+    return True
