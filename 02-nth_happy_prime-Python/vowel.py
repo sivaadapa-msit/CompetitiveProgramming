34b01f3d9943fac094153e640f69042154dd54f5
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-24 14:33:50 +0530

    mod 9 1

---
+++ 02-nth_happy_prime-Python/vowel.py
@@ -0,0 +1,8 @@
+def vowelCount(s):
+    count=0
+    
+    for i in range(len(s)):
+        if("a"==s[i] or "e"==s[i] or "i"==s[i] or "o"==s[i] or "u"==s[i]):
+            count=count+1
+    return count
+print(vowelCount("aercious"))
