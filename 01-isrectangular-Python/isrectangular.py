d5f6d87793542327aaeae938b21ae1dd726c56f7
sivaadapa-msit <sivaadp123@msitprogram.net>
2021-08-30 15:55:13 +0530

    mod 14 1

--- 01-isrectangular-Python/isrectangular.py
+++ 01-isrectangular-Python/isrectangular.py
@@ -4,8 +4,11 @@
 
 
 def fun_isrectangular(l):
-	# Your code goes here...
-	return False
+    # Your code goes here...
+    for i in range(len(l)-1):
+        if(len(l[i])!=len(l[i+1])):
+            return False
+    return True
 
 
 
